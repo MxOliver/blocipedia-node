e26de62f860f1509852e084bc78032368c06da24
Oliver Coley <mxoliverleigh@gmail.com>
2019-02-26 18:09:23 -0500

    user sign up complete

---
+++ src/controllers/userController.js
@@ -0,0 +1,54 @@
+const userQueries = require("../db/queries.users.js");
+const passport = require("passport");
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+module.exports = {
+    signUp(req, res, next){
+        res.render("users/sign_up");
+    },
+    create(req, res, next){
+        let newUser = {
+            email: req.body.email,
+            password: req.body.password,
+            passwordConfirmation: req.body.passwordConfirmation
+        };
+        let messageConfirmation = {
+            to: req.body.email,
+            from: {
+              name: 'Blocipedia',
+              email: 'mxoliver@gmail.com',
+            },
+            subject: 'New User Confirmation',
+            text: 'Your new user registration has been successfully confirmed!',
+            html: '<strong>Your new user registration has been successfully confirmed!</strong>',
+            templateId: "d-7e2b46b75c544d999a21997a569eca2a"
+        }
+        userQueries.createUser(newUser, (err, user) => {
+            if(err){
+                req.flash("error", err);
+                res.redirect("/users/sign_up");
+            } else {
+                passport.authenticate("local")(req, res, () => {
+                req.flash("notice", "You've successfully signed in!");
+                sgMail.send(messageConfirmation);
+                res.redirect("/");
+              })
+            }
+        });
+    },
+    signInForm(req, res, next){
+        res.render("users/sign_in");
+    },
+    signIn(req, res, next){
+        passport.authenticate("local")(req, res, function() {
+            if(!req.user){
+                req.flash("notice", "Sign in failed. Please try again.")
+                res.redirect("/users/sign_in");
+              } else {
+                req.flash("notice", "You've successfully signed in!");
+                res.redirect("/");
+              }
+            })
+    }
+}
